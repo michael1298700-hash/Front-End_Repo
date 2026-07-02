e8d6c3acf8cc0c26927939ab38a12c8e8990f44a
Seraphim <michael1298700@gmail.com>
2026-07-02 20:26:55 +0700

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 'https://tim-anonymous-backend-smarthealth-production.up.railway.app';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://tim-anonymous-backendsmarthealth-production.up.railway.app';
 export default BASE_URL;
